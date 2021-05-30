dde7c8d3a07f5e9c7e671412e9ac4da331ba2b4c
unknown <hodaia0524289665@gmail.com>
2021-05-30 21:35:48 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express=require("express");
+const { Socket } = require("socket.io");
+const app= express();
+const port=process.env.PORT || 5000;
+const http=require("http").createServer();
+
+const io=require("socket.io")(http);
+
+io.on("connection",(socket)=>{
+
+    socket.emit("welcome","welcome to the socket")
+});
+
+http.listen(port,()=>{
+    console.log("server is listening on " +port)
+});
